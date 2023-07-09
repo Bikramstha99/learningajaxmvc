ea3bcd60bf3d78a07d8151694d47be60c45e5765
Bikram Shrestha <stha.bikram999@gmail.com>
2023-07-09 11:16:13 +0545

    get post created

---
+++ AjaxMvc/Models/TransactionDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AjaxMvc.Models
+{
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<TransactionModel> Transactions { get; set; }
+    }
+}
